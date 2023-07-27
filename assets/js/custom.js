d99e20f47907f67e161fb9f55673424b0f522f46
Mr-Rahul-Parmar <parmarrahul536@gmail.com>
2023-07-27 15:05:03 +0530

    remove tawk brand

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -83,3 +83,18 @@ $(document).ready(function () {
       },
    });
 });
+
+setInterval( findTawkAndRemove, 100 );
+
+function findTawkAndRemove() {
+   let parentElement = document.querySelector("iframe[title*=chat]:nth-child(2)");
+
+   if ( parentElement ) {
+
+      let element = parentElement.contentDocument.querySelector(`a[class*=tawk-branding]`);
+
+      if ( element ) {
+         element.remove();
+      }
+   }
+}
